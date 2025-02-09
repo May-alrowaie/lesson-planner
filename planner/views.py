ed92a0aba4a5501ff53c7563a89ad92e517a6579
May-alrowaie <may.alrowaie@gmail.com>
2025-02-09 20:46:13 +0300

    created urls

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse('<h1>Hello Teachers </h1>')
 
-# Create your views here.
+def about(request):
+    return HttpResponse("<h1>About the Lesson planner app</h1>")
